fd80b6fc6ac374c06ded2ee2c2e1a626b2981001
Maurice Bruce Hickling <145118335+Mauriciamo@users.noreply.github.com>
2023-09-25 16:11:44 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+from flask.cli import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ADMINS = ['brudouchoc@gmail.com']
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = 'brudouchoc@gmail.com'
+    MAIL_PASSWORD = 'ipznasohqwxxwhqp'
+    LANGUAGES = [ 'en', 'es' ]
+
