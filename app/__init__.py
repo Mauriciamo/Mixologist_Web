995692c0e76177e4ed5486cba1bd7deb628dd5ef
Maurice Bruce Hickling <145118335+Mauriciamo@users.noreply.github.com>
2023-09-26 14:21:10 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -48,6 +48,30 @@ def get_locale():
         return language
     return request.accept_languages.best_match(app.config['LANGUAGES'].keys())
 
+def create_app(config_class=Config):
+    # ...
+    if not app.debug and not app.testing:
+        # ...
+
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/microblog.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
+
+    return app
 
 from app import routes
 
